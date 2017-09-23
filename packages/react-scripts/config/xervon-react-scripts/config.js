2949c7defbd69946998bdbf9d2ecc4fb420d2e67
Mika Dede <xervondev@gmail.com>
2017-09-23 22:23:29 +0200

    fixed filter of process.env keys

--- packages/react-scripts/config/xervon-react-scripts/config.js
+++ packages/react-scripts/config/xervon-react-scripts/config.js
@@ -4,7 +4,7 @@ module.exports = (isDev = true) => ({
   resolve: {
     alias: require('./customizers/webpackAliases.js')(
       Object.keys(process.env)
-        .filter(e => e.indexOf('REACT_APP_ALIAS_'))
+        .filter(e => e.indexOf('REACT_APP_ALIAS_') > -1)
         .map(e => process.env[e])
     ),
   },
