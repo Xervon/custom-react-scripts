f4d2014384c83119f719c6bf66b308ed37865480
Mika Dede <xervondev@gmail.com>
2017-09-24 01:51:46 +0200

    added option to activate DirectoryNamedWebpackPlugin

--- packages/react-scripts/config/custom-react-scripts/customizers/webpack-plugins.js
+++ packages/react-scripts/config/custom-react-scripts/customizers/webpack-plugins.js
@@ -1,6 +1,10 @@
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 
 module.exports = {
+  DIRECTORY_NAMED: {
+    get: () => new DirectoryNamedWebpackPlugin(),
+  },
   WEBPACK_DASHBOARD: {
     get: () => new DashboardPlugin(),
   },
